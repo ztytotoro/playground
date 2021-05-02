a53936f4c6a4aea68c136296f3488ef470ba10e6
Yarn <ztytotoro@users.noreply.github.com>
2021-05-02 15:08:47 +0800

    update

---
+++ erlang/pattern-func.erl
@@ -0,0 +1,9 @@
+-module(pattern_func).
+
+-export([greet/2]).
+
+greet(male, Name) ->
+    io:format("Hello, Mr. ~s!", [Name]);
+greet(female, Name) ->
+    io:format("Hello, Mrs. ~s!", [Name]);
+greet(_, Name) -> io:format("Hello, ~s!", [Name]).
